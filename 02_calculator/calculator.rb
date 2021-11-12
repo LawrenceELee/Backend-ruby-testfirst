e0431d5c81795a63dd1231d954430568ed7fa025
Lawrence L <Lawrence.E.Lee@gmail.com>
2021-11-11 20:18:07 -0800

    Finished core of 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,13 @@
 #write your code here
+
+def add( num1, num2)
+  return num1 + num2
+end
+
+def subtract( num1, num2 )
+  return num1 - num2
+end
+
+def sum( num_array )
+  return num_array.reduce(0) { |accumulator, element| accumulator + element }
+end
