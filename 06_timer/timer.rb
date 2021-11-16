7dd5d3da020885f43b320e3994e3c7e3646858e5
Lawrence L <Lawrence.E.Lee@gmail.com>
2021-11-15 22:40:34 -0800

    Finished 06_timer. Used ruby printf()

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,35 @@
 class Timer
-  #write your code here
+
+  # when a timer is created without any params,
+  # we set the default number of time/secs to zero
+  def initialize
+    @secs = 0
+  end
+
+  # getter method
+  def seconds
+    @secs
+  end
+
+  # setter method
+  def seconds=(s)
+    @secs = s
+  end
+
+  # takes no params, this converts num of seconds to 
+  # time string in format HH:MM:SS
+  def time_string
+    hours = @secs / 3600    # convert sec -> hr
+    remainder = @secs % 3600
+
+    mins = remainder / 60
+    secs = remainder % 60
+
+    # use Ruby built-in string formatting like printf() in other languages
+    "%02d:%02d:%02d" % [hours, mins, secs]    # returns string in HH:MM:SS format
+  end
+
+
+
+
 end
